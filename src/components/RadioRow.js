99d9fd2cdb158690deade86ead56a7e457a95577
zombico <franz@ombi.co>
2019-11-24 11:11:33 -0500

    type checking for radio row component

--- src/components/RadioRow.js
+++ src/components/RadioRow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class RadioRow extends Component {
   constructor(){ 
@@ -14,30 +15,40 @@ class RadioRow extends Component {
     this.props.handleQ(this.props.id, e.target.value)
   }
 
-render() {
-  const inputfields = this.props.options.map(option => {
-    return(
-      <input
-        className="radios"
-        value={option}
-        type="radio"
-        checked={option == this.state.selection}
-        onChange={this.select}        
-      />  
-    )
-  })
+  render() {
+    const inputfields = this.props.options.map(option => {
+      return(
+        <input
+          className="radios"
+          value={option}
+          type="radio"
+          checked={option == this.state.selection}
+          onChange={this.select}        
+        />  
+      )
+    })
 
-  return(
-    <section className="question-container">
-      <label htmlFor={this.props.id}>{this.props.title}</label>
-      <div className="row-container" id={this.props.id}>
-        <span className="agree">Agree</span>
-        {inputfields}
-        <span className="disagree">Disagree</span>
-      </div>
-    </section>
-  )
+    return(
+      <section className="question-container">
+        <label htmlFor={this.props.id}>{this.props.title}</label>
+        <div className="row-container" id={this.props.id}>
+          <span className="agree">Agree</span>
+          {inputfields}
+          <span className="disagree">Disagree</span>
+        </div>
+      </section>
+    )
+  }
+
+  
 }
+
+RadioRow.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.array,
+  type: PropTypes.string,
+  checked: PropTypes.object,
+  onChange: PropTypes.func
 }
 
 export default RadioRow
